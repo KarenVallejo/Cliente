5ae26492ad0bc9f7e580cc7316c1a1cc05c0393e
KarenVallejo <vanessita-1991@hotmail.com>
2022-04-08 02:12:12 -0500

    Se agrega el proyecto

---
+++ src/test/java/mx/com/gm/clientemain/ViajeTodoIncluidoTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package mx.com.gm.clientemain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author KAREN
+ */
+public class ViajeTodoIncluidoTest {
+    
+    public ViajeTodoIncluidoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of descripcion method, of class ViajeTodoIncluido.
+     */
+    @Test
+    public void testDescripcion() {
+        System.out.println("descripcion");
+        ViajeTodoIncluido instance = null;
+        String expResult = "";
+        String result = instance.descripcion();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
